0b60c200a89ae8bebcfa40dcc6518337a7b2cca1
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-02-02 21:49:22 +0800

    Commit 2 Files.

--- iOS Switch Control/ViewController.swift
+++ iOS Switch Control/ViewController.swift
@@ -8,10 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        if stateSwitch.isOn {
+            textLabel.text = "OFF"
+            stateSwitch.setOn(false, animated:true)
+        } else {
+            textLabel.text = "ON"
+            stateSwitch.setOn(true, animated:true)
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        stateSwitch.addTarget(self, action: #selector(stateChanged), for: .valueChanged)
+    }
+    
+    
+    @objc func stateChanged(switchState: UISwitch) {
+        if switchState.isOn {
+            textLabel.text = "ON"
+            
+        } else {
+            textLabel.text = "OFF"
+        }
     }
 
 
